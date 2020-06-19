92ab9f3f4d9962bd7833f00a2feb290b5840eaaf
Jakson Vilaça <jpv.lic16@uea.edu.br>
2020-06-18 23:11:20 -0400

    Nome do site administrativo alterado

--- resources/views/admin/bem_vindo.blade.php
+++ resources/views/admin/bem_vindo.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
   <h1 class="display-4">Bem Vindo!</h1>
-  <p class="lead">Esta é a página de administração do site do LabTEd.</p>
+  <p class="lead">Esta é a página de administração do site ThinkTEd.</p>
   <hr class="my-4">
 </div>
