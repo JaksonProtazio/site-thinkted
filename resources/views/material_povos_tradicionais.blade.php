127cda5de3386f7e4e0bfe1316f0250bc1ce2374
Jakson Vilaça <jpv.lic16@uea.edu.br>
2020-06-24 13:32:43 -0400

    Correção de texto

--- resources/views/material_povos_tradicionais.blade.php
+++ resources/views/material_povos_tradicionais.blade.php
@@ -8,13 +8,13 @@
 <header>
 	<div id="geralHeader">
 		<div id="cabecalho-povos-tradicionais" class="jumbotron">
-			<h2 id="tit-cab-lin-pesq">Materiais Digitais para<br>Povos Tradiconais</h2>
+			<h2 id="tit-cab-lin-pesq">Tecnologias Educacionais <br> para Povos Indígenas</h2>
 		</div>
 	</div>
 </header>
 	<section id="sobrePovosTradicionais" class="secaoLinhaDePesquisa cor">
 		<h2 class="tituloLinhaDePesquisa">Sobre</h2>
-		<p class="textSobre text-justify">Linha pesquisa em parceria com o laboratório Dabukuri – Planejamento e Gestão do Território da Amazônia – localizado na Universidade Federal do Amazonas – IFCHL/UFAM, que provém a comunicação entre o Laboratório de Tecnologias Educacionais – LabTEd – com os povos indígenas, o projeto tem enfoque na “materialização” e adaptação digital de todo o material produzido pelos mesmos, o que contribui na aplicação da tecnologia em meio indígena, sem que haja qualquer tipo de choque cultural.</p>
+		<p class="textSobre text-justify">Linha pesquisa em parceria com o laboratório Dabukuri – Planejamento e Gestão do Território da Amazônia – localizado na Universidade Federal do Amazonas – IFCHL/UFAM, que provém a comunicação entre o Thinkted Lab - Pesquisa e Desenvolvimento em Tecnologias Educacionais – com os povos indígenas, o projeto tem enfoque na “materialização” e adaptação digital de todo o material produzido pelos mesmos, o que contribui na aplicação da tecnologia em meio indígena, sem que haja qualquer tipo de choque cultural.</p>
 	</section>
 
 	<section id="projetosPovosTradicionais" class="secaoLinhaDePesquisa">
